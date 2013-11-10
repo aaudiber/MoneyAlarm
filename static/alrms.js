0a66f1511e0ab660dd067559179103ec5e24d967
Peter Xiao <pterxiao@gmail.com>
2013-11-10 00:00:21 -0500

    asdfjalsdkfjasl;dfj

---
+++ static/alrms.js
@@ -0,0 +1,11 @@
+$.get('alarms', function(data) {
+  console.log(data);
+  var alarms = JSON.parse(data);
+  var $alarmsList = $('#alarms-list');
+  for(var i = 0; i < alarms.length; i++) {
+    var listItem = document.createElement('li');
+    listItem.content = alarms[i];
+    listItem.class = 'noon';
+    $alarmsList.append(listItem);
+  }
+});
