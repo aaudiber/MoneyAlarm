a773d6e22edaf6d3b36edde25aba76275b1abd14
Peeta Malloc <pterx@donhuang.me>
2013-11-10 05:48:25 +0000

    titty bitch

--- static/alrms.js
+++ static/alrms.js
@@ -4,9 +4,30 @@ $(document).ready(function() {
     var alarms = JSON.parse(data);
     var $alarmsList = $('#alarms-list');
     for(var i = 0; i < alarms.length; i++) {
+      var date = new Date(alarms[i] * 1000);
       var listItem = document.createElement('li');
-      listItem.innerHTML = alarms[i];
-      listItem.className = 'noon';
+      listItem.innerHTML = date.toLocaleTimeString('en-US');
+      if (date.getHours() < 6) {
+        listItem.className = 'night';
+      } else if (date.getHours() < 7) {
+        listItem.className = 'sunrise';
+      } else if (date.getHours() < 12) {
+        listItem.className = 'morning';
+      } else if (date.getHours() < 13) {
+        listItem.className = 'noon';
+      } else if (date.getHours() < 14) {
+        listItem.className = 'early-afternoon';
+      } else if (date.getHours() < 16) {
+        listItem.className = 'mid-afternoon';
+      } else if (date.getHours() < 18) {
+        listItem.className = 'late-afternoon';
+      } else if (date.getHours() < 19) {
+        listItem.className = 'dusk';
+      } else if (date.getHours() < 20) {
+        listItem.className = 'early-night';
+      } else {
+        listItem.className = 'night';
+      }
       $alarmsList.append(listItem);
     }
   });
